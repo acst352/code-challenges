947bf53426c2b13a010b1e918a224a8aa60c9b66
Alex Castrejón <acst352@gmail.com>
2023-08-05 23:55:20 -0600

    Update sortArray.js

--- js/sortArray.js
+++ js/sortArray.js
@@ -1,8 +1,21 @@
 const exampleArray = [42, -17, 0, "a", 3.14, "!", "b", -9, "@", 7, "#", "c", -23];
 
-
 function sortArray() {
-  document.write(exampleArray.sort());
+  // Create a sorted copy of the exampleArray without modifying the original
+  const sortedArray = exampleArray.slice().sort((a, b) => {
+    if (typeof a === "number" && typeof b === "number") {
+      return a - b; // Sort numbers in ascending order
+    } else if (typeof a === "string" && typeof b === "string") {
+      return a.localeCompare(b); // Sort strings alphabetically
+    } else if (typeof a === "number") {
+      return -1; // Numbers before strings
+    } else {
+      return 1; // Strings after numbers
+    }
+  });
+
+  // Display the sorted array
+  document.write(sortedArray);
 }
 
 sortArray();
