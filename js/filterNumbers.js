0d23b20241a2d9fd4d6168621b68802a83285064
acst352 <acst352@gmail.com>
2023-08-07 23:00:40 -0600

    Print original and modified array

--- js/filterNumbers.js
+++ js/filterNumbers.js
@@ -1,8 +1,10 @@
-const anArray = [42, -17, 0, 3.14, -9, 7, -23, 100, -50];
+const numbers = [42, -17, 0, 3.14, -9, 7, -23, 100, -50];
 
-function filterNumbers() {
-  let result = anArray.filter((number) => number >= 0);
-  document.write(result);
-}
+// Filter negative numbers
+const filteredNumbers = numbers.filter((number) => number >= 0);
 
-filterNumbers();
+const originalArrayElement = document.getElementById("originalArray");
+originalArrayElement.textContent = numbers.join(", ");
+
+const modifiedArrayElement = document.getElementById("modifiedArray");
+modifiedArrayElement.textContent = filteredNumbers.join(", ");
