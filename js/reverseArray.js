b9acd2349d572c4a52698718b0c3dd3836dd0051
Alex Castrejón <acst352@gmail.com>
2023-08-10 23:53:00 -0600

    Print original and reversed array

--- js/reverseArray.js
+++ js/reverseArray.js
@@ -1,10 +1,10 @@
 const arrayay = [0, 8, 5, 3, 9, 5, 7, 6, 9, 2, 6, 1, 8, 4, 7, 5, 6, 3, 7];
 
-const contenedor = document.getElementById("original-array");
-const originalHTML = "Original Array = " + arrayay;
-contenedor.innerHTML = originalHTML;
-
-const contenedor2 = document.getElementById("reversed-array");
+// Reverse array
 const reversedArray = arrayay.slice().reverse();
-const newHTML = "Reversed Array = " + reversedArray;
-contenedor2.innerHTML = newHTML;
+
+const originalArray5 = document.getElementById("originalArray5");
+originalArray5.textContent = arrayay.join(", ");
+
+const modifiedArray5 = document.getElementById("reversedArray5");
+modifiedArray5.textContent = reversedArray.join(", ");
