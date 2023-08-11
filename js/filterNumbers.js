34e08c19069e10b0b9db06e2487ab50ef93d7e98
Alex Castrejón <acst352@gmail.com>
2023-08-10 23:53:33 -0600

    Update filterNumbers.js

--- js/filterNumbers.js
+++ js/filterNumbers.js
@@ -3,8 +3,8 @@ const numbers = [42, -17, 0, 3.14, -9, 7, -23, 100, -50];
 // Filter negative numbers
 const filteredNumbers = numbers.filter((number) => number >= 0);
 
-const originalArrayElement = document.getElementById("originalArray");
-originalArrayElement.textContent = numbers.join(", ");
+const originalArray7 = document.getElementById("originalArray7");
+originalArray7.textContent = numbers.join(", ");
 
-const modifiedArrayElement = document.getElementById("modifiedArray");
-modifiedArrayElement.textContent = filteredNumbers.join(", ");
+const modifiedArray7 = document.getElementById("modifiedArray7");
+modifiedArray7.textContent = filteredNumbers.join(", ");
