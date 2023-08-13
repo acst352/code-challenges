0778d10f97a699a5915b44e31b3087e812664e81
acst352 <acst352@gmail.com>
2023-08-13 11:18:48 -0600

    Update lengthConverter function

--- js/lengthConverter.js
+++ js/lengthConverter.js
@@ -1,16 +1,10 @@
-// voy a convertir de km a mi.
-// Quiero saber cuánto son 5 km en mi.
-// Entonces número de km * 0.621371.
-// Entonces 5 x 0.621371 = 3.1 mi.
-
-// 1 km = 0.621371 mi.
-// 1 mi = 1.609344 km.
-
 function kmToMi(km) {
   let mi = 0.621371;
   let result = km * mi;
-
-  document.write(km + " km" + " = " + result + " mi");
+  return km + " km" + " = " + result + " mi";
 }
 
 kmToMi(1);
+
+const printResult = document.getElementById("lengthConverter");
+printResult.textContent = kmToMi(1);
