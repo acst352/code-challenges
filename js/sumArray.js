e5f770b02c03e69d2c4df41bd8b646e47bf96870
acst352 <acst352@gmail.com>
2023-08-13 11:46:30 -0600

    Update sumArray function

--- js/sumArray.js
+++ js/sumArray.js
@@ -2,12 +2,13 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
 function sumArray(numbers) {
   let sum = 0;
-  // Using for...of statement is recommended as simplifies the syntax and eliminates the need for a counter varaible.
+  // Using for...of statement is recommended as simplifies the syntax and eliminates the need for a counter variable.
   for (const element of numbers) {
     sum += element;
   }
   return sum;
 }
 
-const result = sumArray(array);
-document.write(array + " = " + result);
+const result = `${array} = ${sumArray(array)}`;
+const printSum = document.getElementById("sumArray");
+printSum.textContent = result;
